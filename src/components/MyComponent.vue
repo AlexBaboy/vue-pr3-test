0af5c59d31077b7bc768e74e1570f4e347202763
Alex Baboy <alexbaboy2016@gmail.com>
2021-08-12 23:42:37 +0300

    first!

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="root">
+    <label>
+      Введите еmail:
+      <input type="email"
+             :value="email"
+             @input="emailInput($event.target.value)"
+      />
+    </label>
+    <label>
+      Введите пароль:
+      <input type="password"
+             :value="password"
+             @input="passwordInput($event.target.value)"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      email: 'example@mail.ru',
+      password: '12345'
+    }
+  },
+  methods: {
+    emailInput(value) {
+      this.email = value
+    },
+    passwordInput(value) {
+      console.log(value)
+      this.password = value
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
